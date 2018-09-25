4c908f501be7a9a6b6622c5396bb591af88b88f4
ketangote <ketan.gote@metamagic.in>
2018-09-25 15:35:52 +0530

    Made app war compatible

--- productms/src/main/java/com/metamagic/productms/ProductMsApplication.java
+++ productms/src/main/java/com/metamagic/productms/ProductMsApplication.java
@@ -2,11 +2,19 @@ package com.metamagic.productms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ProductMsApplication {
+public class ProductMsApplication extends SpringBootServletInitializer{
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProductMsApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ProductMsApplication.class);
 	}
 }
