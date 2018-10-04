14f6d1f2b9de5b3cefd3fdced70507c086987f5d
ketangote <ketan.gote@metamagic.in>
2018-10-04 19:28:23 +0530

    Changes Added Istio

---
+++ k8_workshop_istio_v1.0.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+# Copyright (c) METAMAGIC GLOBAL INC, USA
+#
+# This script will create the database setup
+#
+
+echo "Create Shoppingportal namespace"
+kubectl create -f https://raw.githubusercontent.com/meta-magic/kubernetes_workshop/master/yaml/infra/shoppiny-ns.yaml
+
+echo "Creating ingress"
+kubectl create -f https://raw.githubusercontent.com/meta-magic/kubernetes_workshop/master/yaml/infra/shopping-ingress.yaml
+
+echo "Creating MYSQL(secret, volumn, deployment and service) in k8"
+kubectl create -f https://raw.githubusercontent.com/meta-magic/kubernetes_workshop/master/yaml/mysqlfiles/mysql-secret.yaml
+kubectl create -f https://raw.githubusercontent.com/meta-magic/kubernetes_workshop/master/yaml/mysqlfiles/mysql-pv.yaml
+kubectl create -f https://raw.githubusercontent.com/meta-magic/kubernetes_workshop/master/yaml/mysqlfiles/mysql-pvc.yaml
+kubectl create -f https://raw.githubusercontent.com/meta-magic/kubernetes_workshop/master/yaml/mysqlfiles/mysql-dep.yaml
+kubectl create -f https://raw.githubusercontent.com/meta-magic/kubernetes_workshop/master/yaml/mysqlfiles/mysql-svc.yaml
+
+echo "Create Prodcut Microservice secret, deployment and service in k8"
+kubectl create -f https://raw.githubusercontent.com/meta-magic/kubernetes_workshop/master/yaml/microservice/product/product-secret.yaml
+kubectl create -f https://raw.githubusercontent.com/meta-magic/kubernetes_workshop/master/yaml/microservice/product/product-deployment.yaml
+kubectl create -f https://raw.githubusercontent.com/meta-magic/kubernetes_workshop/master/yaml/microservice/product/product-service.yaml
+
+echo "Create Prodcut Review Microservice deployment and service in k8"
+kubectl create -f https://raw.githubusercontent.com/meta-magic/kubernetes_workshop/master/yaml/microservice/productreview/productreview-deployment.yaml
+kubectl create -f https://raw.githubusercontent.com/meta-magic/kubernetes_workshop/master/yaml/microservice/productreview/productreview-service.yaml
+
+echo "Create UI deployment and service in k8 for version 1.0"
+kubectl create -f https://raw.githubusercontent.com/meta-magic/kubernetes_workshop/master/yaml/ui/k8uiworkshop-deploymentv1.0.yaml
+kubectl create -f https://raw.githubusercontent.com/meta-magic/kubernetes_workshop/master/yaml/ui/k8uiworkshop-service.yaml
+
+
+
+echo $PWD
+
