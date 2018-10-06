74932301e06cf10b3a4c9dab3d06aa6e803e281f
deepali-arvind <deepali.arvind@metamagic.in>
2018-10-06 14:28:18 +0530

    add aws_istio.sh

---
+++ aws_istio.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#
+# Copyright (c) METAMAGIC GLOBAL INC, USA
+#
+# This script will create the database setup
+#
+
+echo " MAKE SURE YOU HAVE EXECUTED BELOW YAML BEFORE..."
+
+echo "install/kubernetes/helm/istio/templates/crds.yaml && install/kubernetes/istio-demo.yaml"
+
+
+
+echo "Create Shoppingportal namespace"
+kubectl replace -f https://raw.githubusercontent.com/meta-magic/kubernetes_workshop/master/yaml/istio/aws-istio/shopping-ns.yaml
+
+echo "Creating MYSQL(secret, volumn, deployment and service) in k8"
+kubectl create -f https://raw.githubusercontent.com/meta-magic/kubernetes_workshop/master/yaml/istio/aws-istio/mysqlfiles/mysql-secret.yaml
+kubectl create -f https://raw.githubusercontent.com/meta-magic/kubernetes_workshop/master/yaml/istio/aws-istio/mysqlfiles/mysql-pv.yaml
+kubectl create -f https://raw.githubusercontent.com/meta-magic/kubernetes_workshop/master/yaml/istio/aws-istio/mysqlfiles/mysql-pvc.yaml
+kubectl create -f https://raw.githubusercontent.com/meta-magic/kubernetes_workshop/master/yaml/istio/aws-istio/mysqlfiles/mysql-dep.yaml
+kubectl create -f https://raw.githubusercontent.com/meta-magic/kubernetes_workshop/master/yaml/istio/aws-istio/mysqlfiles/mysql-svc.yaml
+
+echo "Create Product Microservice deployment and service in k8"
+kubectl create -f https://raw.githubusercontent.com/meta-magic/kubernetes_workshop/master/yaml/istio/aws-istio/product/product-v1.yaml
+kubectl create -f https://raw.githubusercontent.com/meta-magic/kubernetes_workshop/master/yaml/istio/aws-istio/product/product-v2.yaml
+kubectl create -f https://raw.githubusercontent.com/meta-magic/kubernetes_workshop/master/yaml/istio/aws-istio/product/product-service.yaml
+kubectl create -f https://raw.githubusercontent.com/meta-magic/kubernetes_workshop/master/yaml/istio/aws-istio/product/product-destination.yaml
+
+
+echo "Create Product Review Microservice deployment and service in k8"
+kubectl create -f https://raw.githubusercontent.com/meta-magic/kubernetes_workshop/master/yaml/istio/aws-istio/productreview/productreview-v1.yaml
+kubectl create -f https://raw.githubusercontent.com/meta-magic/kubernetes_workshop/master/yaml/istio/aws-istio/productreview/productreview-service.yaml
+
+echo "Create UI deployment and service in k8"
+kubectl create -f https://raw.githubusercontent.com/meta-magic/kubernetes_workshop/master/yaml/istio/aws-istio/ui/ui-v1.yaml
+kubectl create -f https://raw.githubusercontent.com/meta-magic/kubernetes_workshop/master/yaml/istio/aws-istio/ui/ui-v2.yaml
+kubectl create -f https://raw.githubusercontent.com/meta-magic/kubernetes_workshop/master/yaml/istio/aws-istio/ui/ui-destination.yaml
+kubectl create -f https://raw.githubusercontent.com/meta-magic/kubernetes_workshop/master/yaml/istio/aws-istio/ui/ui-service.yaml
+
+echo "Create Gateway  in k8"
+kubectl create -f https://raw.githubusercontent.com/meta-magic/kubernetes_workshop/master/yaml/istio/aws-istio/shoppingportal-gw.yaml
+
+echo "Create Virtual Service in k8"
+kubectl create -f https://raw.githubusercontent.com/meta-magic/kubernetes_workshop/master/yaml/istio/aws-istio/shoppingportal-virtualservice.yaml
+
+
+
+
+echo $PWD
+
