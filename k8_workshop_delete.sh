5d87ab32d42e3df21ab7b914cb37bc1351d8da6b
ketangote <ketan.gote@metamagic.in>
2018-10-08 13:08:41 +0530

    Changes: Update Script

---
+++ k8_workshop_delete.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#
+# Copyright (c) METAMAGIC GLOBAL INC, USA
+#
+# This script will create the database setup
+#
+#!/bin/bash
+#
+# Copyright (c) METAMAGIC GLOBAL INC, USA
+#
+# This script will create the database setup
+#
+
+echo " MAKE SURE YOU HAVE EXECUTED BELOW YAML BEFORE..."
+
+echo "Delete Virtual Service in k8"
+kubectl delete -f https://raw.githubusercontent.com/meta-magic/kubernetes_workshop/master/yaml/istio/shoppingportal-virtualservice.yaml
+
+echo "Delete Gateway  in k8"
+kubectl delete -f https://raw.githubusercontent.com/meta-magic/kubernetes_workshop/master/yaml/istio/shoppingportal-gw.yaml
+
+
+echo "Delete UI deployment and service in k8"
+kubectl delete -f https://raw.githubusercontent.com/meta-magic/kubernetes_workshop/master/yaml/istio/ui/ui-v1.yaml
+kubectl delete -f https://raw.githubusercontent.com/meta-magic/kubernetes_workshop/master/yaml/istio/ui/ui-v2.yaml
+kubectl delete -f https://raw.githubusercontent.com/meta-magic/kubernetes_workshop/master/yaml/istio/ui/ui-destination.yaml
+kubectl delete -f https://raw.githubusercontent.com/meta-magic/kubernetes_workshop/master/yaml/istio/ui/ui-service.yaml
+
+echo "Delete Product Review Microservice deployment and service in k8"
+kubectl delete -f https://raw.githubusercontent.com/meta-magic/kubernetes_workshop/master/yaml/istio/productreview/productreview-v1.yaml
+kubectl delete -f https://raw.githubusercontent.com/meta-magic/kubernetes_workshop/master/yaml/istio/productreview/productreview-service.yaml
+
+echo "Delete Product Microservice deployment and service in k8"
+kubectl delete -f https://raw.githubusercontent.com/meta-magic/kubernetes_workshop/master/yaml/istio/product/product-v1.yaml
+kubectl delete -f https://raw.githubusercontent.com/meta-magic/kubernetes_workshop/master/yaml/istio/product/product-v2.yaml
+kubectl delete -f https://raw.githubusercontent.com/meta-magic/kubernetes_workshop/master/yaml/istio/product/product-service.yaml
+kubectl delete -f https://raw.githubusercontent.com/meta-magic/kubernetes_workshop/master/yaml/istio/product/product-destination.yaml
+
+echo "Delete MYSQL(secret, volumn, deployment and service) in k8"
+kubectl delete -f https://raw.githubusercontent.com/meta-magic/kubernetes_workshop/master/yaml/mysqlfiles/mysql-secret.yaml
+kubectl delete -f https://raw.githubusercontent.com/meta-magic/kubernetes_workshop/master/yaml/mysqlfiles/mysql-pv.yaml
+kubectl delete -f https://raw.githubusercontent.com/meta-magic/kubernetes_workshop/master/yaml/mysqlfiles/mysql-pvc.yaml
+kubectl delete -f https://raw.githubusercontent.com/meta-magic/kubernetes_workshop/master/yaml/mysqlfiles/mysql-dep.yaml
+kubectl delete -f https://raw.githubusercontent.com/meta-magic/kubernetes_workshop/master/yaml/mysqlfiles/mysql-svc.yaml
+
+echo "Delete Shoppingportal namespace"
+kubectl delete -f https://raw.githubusercontent.com/meta-magic/kubernetes_workshop/master/yaml/istio/shopping-ns.yaml
