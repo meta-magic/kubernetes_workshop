672b7a0f95585b151687c88c400b595fdff47055
deepali-arvind <deepali.arvind@metamagic.in>
2018-10-09 12:10:10 +0530

    Create docker.sh

---
+++ docker.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+sudo apt-get -y update
+sudo apt-get -y install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo apt-key fingerprint 0EBFCD88
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get -y update
+
+sudo apt-get -y install docker-ce
+
+sudo docker run hello-world
+
+
+sudo groupadd docker
+
+sudo usermod -aG docker $1
+
+sudo reboot
+
+
+
+
+
