db8e77454029942a593ed46b73d84a2c9fd6cb89
Araf Karsh Hamid <araf.karsh@gmail.com>
2018-10-17 03:24:05 +0530

    Added Dynamic Storage for PVC

--- yaml/volume-local/cleanup.sh
+++ yaml/volume-local/cleanup.sh
@@ -2,7 +2,9 @@
 kubectl delete sc omega-sc-local
 kubectl delete pv omega-volume-local
 kubectl delete pvc omega-pvc-local -n omega-local
+kubectl delete pvc omega-pvc-local-dynamic -n omega-local
 kubectl delete deploy omega-local-deploy -n omega-local
+kubectl delete deploy omega-local-deploy-dynamic -n omega-local
 kubectl get sc,pv,pvc,pods -n omega-local
 kubectl delete ns omega-local
 kubectl get ns
--- yaml/volume-local/createLocalVolume.sh
+++ yaml/volume-local/createLocalVolume.sh
@@ -3,5 +3,7 @@ kubectl create -f v-local-omega-ns.yaml
 kubectl create -f v-local-storage.yaml
 kubectl create -f v-local-volume.yaml
 kubectl create -f v-local-pvc.yaml
+kubectl create -f v-local-pvc-dynamic.yaml
 kubectl create -f v-local-dep.yaml
+kubectl create -f v-local-dep-dynamic.yaml
 kubectl get sc,pv,pvc,pods -n omega-local
