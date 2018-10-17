87e847087e50c611fd7f0eb6967ded47934f1008
deepali-arvind <deepali.arvind@metamagic.in>
2018-10-17 15:40:53 +0530

    Update cleanup.sh

--- yaml/volume-aws/cleanup.sh
+++ yaml/volume-aws/cleanup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-kubectl delete -f https://raw.githubusercontent.com/meta-magic/kubernetes_workshop/master/yaml/volume-aws/v-omega-ns.yaml
 kubectl delete -f https://raw.githubusercontent.com/meta-magic/kubernetes_workshop/master/yaml/volume-aws/v-aws-storage.yaml
 kubectl delete -f https://raw.githubusercontent.com/meta-magic/kubernetes_workshop/master/yaml/volume-aws/v-aws-volume.yaml
 kubectl delete -f https://raw.githubusercontent.com/meta-magic/kubernetes_workshop/master/yaml/volume-aws/v-aws-pvc.yaml
 kubectl delete -f https://raw.githubusercontent.com/meta-magic/kubernetes_workshop/master/yaml/volume-aws/v-omega-dep.yaml
+kubectl delete -f https://raw.githubusercontent.com/meta-magic/kubernetes_workshop/master/yaml/volume-aws/v-omega-ns.yaml
+
